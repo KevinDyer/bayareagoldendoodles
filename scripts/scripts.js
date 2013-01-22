e76ea62ebd65f7e627581748c4eb19839c6b8cf7
kevin <kevin.m.dyer@gmail.com>
2013-01-21 22:22:09 -0700

    Ryan is a genius.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
-  $("img").mouseenter(function(){
-    $(this).stop(true,false);
-    $(this).animate({
-      width:'40%'
+  $("img")
+    .mouseenter(function(){
+      var $this = $(this);
+      $this
+	    .stop(true,false)
+	    .closest(".puppy")
+		  .css('z-index', '1')
+		  .end()
+        .animate({width:'50%'});
+    })
+    .mouseleave(function(){
+      var $this = $(this);
+      $this
+	    .stop(true,false)
+        .animate({width:'30%'},'slow',function(){
+	      $this
+		    .closest(".puppy")
+			  .css('z-index', '0');
+	    });
     });
-  });
-  $("img").mouseleave(function(){
-    $(this).stop(true,false);
-    $(this).animate({
-      width:'20%'
-    });
-  });
 });
